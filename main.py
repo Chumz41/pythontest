565362494fc7ba5a1f99c4001edb4319660fdace
Joe <jdiaz57@my.nl.edu>
2023-10-03 13:03:05 -0700

    String test

--- main.py
+++ main.py
@@ -12,3 +12,12 @@ age=input("How old are you" + first_name + '?')
 print("Hello", first_name, last_name, "nice to meet you!")
 print(first_name, 'you were born in the year', 2023-int(age))
 
+print("Today is a good day to learn Python")
+print("Python is fun")
+print("Python's string are easy to use")
+print("hello"+"world")
+greeting=("hello")
+name=input("Please enter your name")
+print(greeting+name)
+# if we want a space, we can add that to
+print(greeting+ ' ' +name)
