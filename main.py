e3d693d75f82f4b5677b8b210ec9a7cd18c00cd5
Joe <jdiaz57@my.nl.edu>
2023-10-03 11:59:35 -0700

    input test code

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ print()
 print("The end")
 # First comment in vs Code
 # second comment
-x="userinput"
-print(x)
+
+first_name=input("Please enter your first name:")
+last_name=input("Please enter your last name:")
+age=input("How old are you" + first_name + '?')
+print("Hello", first_name, last_name, "nice to meet you!")
+print(first_name, 'you were born in the year', 2023-int(age))
+
