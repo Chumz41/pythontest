8b3a2def397e73e6723fbc41296a1b2ff8f93d3f
Chumz41 <146126910+Chumz41@users.noreply.github.com>
2023-10-10 14:08:24 -0500

    Add files via upload

---
+++ EscapecharacterCheck.py
@@ -0,0 +1,14 @@
+splitString = "This string has been\nSplit over\nseveral\nlines"
+# Fixed the quotation mark
+print(splitString)
+tabbedString = "1\t2\t3\t4\t5"
+# Fixed the quotation mark
+print(tabbedString)
+print('The pet shop owner said "No, no, \'e\'s uh,...he\' resting".')
+print("The pet shop owner said \"No, no, 'e's uh,...he's resting\".")
+print(''''The Pet shop owner said "No , no 'e's uh,...he's resting".''''')
+anotherString = ''''This string has been \
+    split over \
+    several \
+        lines'''' '# Fixed the quotation mark
+print(anotherString)
